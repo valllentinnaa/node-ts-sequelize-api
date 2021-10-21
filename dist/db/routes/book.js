80e8084ae669cd54d844453043b3218f369780c1
Valentina Valkova <37929416+valllentinnaa@users.noreply.github.com>
2021-10-21 21:08:58 +0300

    Delete dist directory

--- dist/db/routes/book.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var book_1 = require("../controllers/book");
-var router = (0, express_1.Router)();
-router.post('/', book_1.createBook);
-router.get('/', book_1.getBook);
-exports.default = router;
